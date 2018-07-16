784767922aa12d9bdbb6a3e5c0ce50b498978119
Natalie Chow <39046935+natc66@users.noreply.github.com>
2018-07-16 18:55:46 -0400

    initial

---
+++ etc2.js
@@ -0,0 +1,46 @@
+"use strict";
+(function () {
+    let currentTab = "graphic";
+    let currentContent = "graphicDesign"
+    
+    window.onload = function () {
+        getImages ();
+        $("open").onclick = openMenu;
+    };
+    
+    function getImages () {
+        let url = "etc.php";
+        fetch(url, {credentials: "include"})
+            .then(checkStatus)
+            .then(JSON.parse)
+            .then(loadImages)
+            .catch(function (error) {
+                alert(error);
+            });
+    }
+    
+    function loadImages (data) {
+        alert(data);
+        for (let i = 0; i < data.length; i++) {
+            
+        }
+    }
+    
+    function checkStatus(response) {
+        if (response.status >= 200 && response.status < 300) {
+            return response.text();
+        } else {
+            return Promise.reject(new Error(response.status +
+            ": " + response.statusText));
+        }
+    }
+    
+    function $ (id) {
+        return document.getElementById(id);
+    }
+    
+    function qsa (selector) {
+        return document.querySelectorAll(selector);
+    }
+    
+}) ();
